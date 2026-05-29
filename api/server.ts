c39aa1a038a8f323f877331adaa81a5d0e68b4d6
rehanmirza76 <abdulrehan0317@gmail.com>
2026-05-29 17:48:29 +0500

    added vercel.json

---
+++ api/server.ts
@@ -0,0 +1,9 @@
+import server from "../dist/server/server.js";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(request: Request) {
+  return server.fetch(request, undefined, undefined);
+}
